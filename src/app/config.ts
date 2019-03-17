c35a1b3607ac37f20c6195400eb1dbd390bf693e
kushalvats5 <ksharma.sharma27@gmail.com>
2019-03-17 22:19:34 +0530

    front api

--- src/app/config.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@angular/core";
-
-@Injectable()
-export class Config {
-  apiPath: string = 'http://localhost/lab_backapi/';
-  //apiPath: string = 'http://lab.bintechsol.com/backend/';
-}
